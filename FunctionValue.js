dc8f88c66f5c6a81b2688c86e570130968ad3a80
Dmitry734 <115181253+Dmitry734@users.noreply.github.com>
2024-03-29 13:55:10 +0100

    js

---
+++ FunctionValue.js
@@ -0,0 +1,12 @@
+
+function AddToMyNumber(num1, num2) {
+    num1++;
+    num2++;
+    console.log('Digit in function num1:' + num1);
+    console.log('Digit in function num2:' + num2);
+};
+let num1 = 1;
+let num2 = 2;
+AddToMyNumber(num1, num2);
+console.log('true value of Digit num1:' + num1);
+console.log('true value of Digit num2:' + num2);
